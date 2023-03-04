4c6cf736b3227c9a774f30f00c5849ea8c75209b
saimohanreddy98 <saimohanreddy98@gmail.com>
2023-03-04 17:53:12 +0530

    doneee

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -1,31 +1,33 @@
-import React,{useRef} from "react";
-import {bank1} from "./App"
+import React, { useRef } from "react";
+import { bank1 } from "./App";
 
-function Pad({handleClick, power, backgroundStyle, element, id }) {
-  const audio=newAudio(bank1[element].source);
+function Pad({ handleClick, power, backgroundStyle, element, id }) {
+  const audio = new Audio(bank1[element].source);
 
-  window.addEventListener("keydown",(event)=>{
-    if(event.key===element || event.key ===element.toLowerCase()){
+  window.addEventListener("keydown", (event) => {
+    if (event.key === element || event.key === element.toLowerCase()) {
       handleClick(bank1[element]);
       audio.play();
     }
   });
-    return (
-      <button 
-        data-tag={id}
-        type='button'
-        className='drum-pad' 
-        onClick={()=>{
-          audio.play();
-          handleClick(bank1[element]);
-        }} 
-        id={bank1[element]} 
-        disabled={!power} 
-        style={{background: `${backgroundStyle}`}}>
-        {element}
-        <audio id={element} src={bank1[element].source} className='clip'></audio>
-      </button>
-    );
-  }
-  
-  export default Pad ; 
+
+  return (
+    <button
+      data-tag={id}
+      type="button"
+      className="drum-pad"
+      onClick={() => {
+        audio.play();
+        handleClick(bank1[element]);
+      }}
+      id={bank1[element]}
+      disabled={!power}
+      style={{ background: `${backgroundStyle}` }}
+    >
+      {element}
+      <audio id={element} src={bank1[element].source} className="clip"></audio>
+    </button>
+  );
+}
+
+export default Pad;
